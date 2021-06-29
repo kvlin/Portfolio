659e617437cc8cd3faac6148a2bee909821a857c
kvlin <kevlin129@hotmail.com>
2021-06-29 23:26:41 +1000

    Adding Gymtrack project to the porfolio page

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -6,15 +6,16 @@ import poGenAddProductImg from '../../images/po-generator/All-pages-screenshot.P
 import poGenCalculatorImg from '../../images/po-generator/Calculation-page-screenshot.PNG'
 import poSampleImg from '../../images/po-generator/Sample-PDF-screenshot.PNG'
 import budgetTracker1 from '../../images/budget-tracker/iphone11-screenshot-5.png'
+import gymTrackImg from '../../images/gymtrack/All.PNG'
 
 export default function Main() {
   const poGenerator = {
     title: 'Purchase Order Generator - SaaS',
-    summary: 'SaaS application built to automate part of my employment routine task. For a given pharmaceutical product and quantity, the user will be able to obtain purchase orders for every single raw material needed within matters of seconds, minimizing the need for manual input, resulting in greater productivity.',
+    summary: "A SaaS built to automate part of my employment routine task. Upon providing the product and it's quantity, purchase order PDF's can be obtained for all raw materials within matters of seconds, significantly reducing the need for manual data entries.",
     link: 'https://github.com/kvlin/PurchaseOrder-Generator',
     anchor: 'More info',
-    technologies: "the MERN stack (MongoDB, ExpressJS, React, NodeJS), Ant Design",
-    last_technology: 'Passport.js (authentication)',
+    technologies: "the MERN stack (MongoDB, ExpressJS, React, NodeJS), Ant Design, Passport.js, Passport.js (authentication)",
+    last_technology: 'Bcrypt',
     images:[ 
       {image:poGenAddProductImg, caption: "Purchase Order Generator", style:{ maxWidth:"1660px", width: "auto",height: "auto"}},
       {image:poGenCalculatorImg, style:{ maxWidth:"660px", width: "auto",height: "auto"}},
@@ -27,8 +28,9 @@ export default function Main() {
     summary: 'This gym application incorporates authentication to render different UI and UX depending on the type of user authenticated - gym instructors can create classes, and members can book the classes and leave reviews.',
     link: 'https://github.com/kvlin/gymtrack',
     anchor: 'More info',
-    technologies: 'MySQL, Javascipt Handlebars',
-    last_technology: 'deployed on Heroku'
+    technologies: 'MySQL, Javascipt Handlebars, Passport.js, Bcrypt',
+    last_technology: 'deployed on Heroku',
+    images:[{image:gymTrackImg, caption: "Gymtrack - An app for Gym classes creation and reservation", style:{ maxWidth:"1660px", width: "auto",height: "auto"}}]
     
   }
   const travlmate = {
@@ -39,7 +41,7 @@ export default function Main() {
   }
   const budgetTracker = {
     title: 'Budget Tracker - Progressive Web App',
-    summary: 'The application allows users to log their withdrawals or deposits for budget tracking. It works even when the device is offline and will synchronize the data with the cloud database upon regaining connection. This is particularly useful when for travellers who visit remote places where internet may not be available.',
+    summary: 'The application allows users to log their withdrawals or deposits for budget tracking. It works even when the device is offline and will synchronize the data with the cloud database upon regaining connection. This is particularly useful for travellers who visit regions with spasmodic internet availability.',
     link: 'https://github.com/kvlin/Budget-Tracker',
     anchor: 'More info',
     technologies: "IndexedDB, Service Worker, Cache API",
@@ -69,10 +71,10 @@ export default function Main() {
           <Col style={{marginTop:"2em"}}>
             <ProjectsBrief>{budgetTracker}</ProjectsBrief>
           </Col >
-          {/* <Col style={{marginTop:"2em"}}>
+          <Col style={{marginTop:"2em"}}>
             <ProjectsBrief>{gymTrack}</ProjectsBrief>
           </Col >
-          <Col style={{marginTop:"2em"}}>
+          {/* <Col style={{marginTop:"2em"}}>
             <ProjectsBrief>{fitnessTracker}</ProjectsBrief>
           </Col >
           <Col style={{marginTop:"2em"}}>
