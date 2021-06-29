7a2ff00d2a41cd7508b2072bcb03494a5dee9c9e
kvlin <kevlin129@hotmail.com>
2021-06-29 23:52:44 +1000

    Editing project brief texts

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -14,7 +14,7 @@ export default function Main() {
     summary: "A SaaS built to automate part of my employment routine task. Upon providing the product and it's quantity, purchase order PDF's can be obtained for all raw materials within matters of seconds, significantly reducing the need for manual data entries.",
     link: 'https://github.com/kvlin/PurchaseOrder-Generator',
     anchor: 'More info',
-    technologies: "the MERN stack (MongoDB, ExpressJS, React, NodeJS), Ant Design, Passport.js, Passport.js (authentication)",
+    technologies: "the MERN stack (MongoDB, Express, React, Node.js), Ant Design, jsPDF, Passport.js (authentication)",
     last_technology: 'Bcrypt',
     images:[ 
       {image:poGenAddProductImg, caption: "Purchase Order Generator", style:{ maxWidth:"1660px", width: "auto",height: "auto"}},
@@ -28,23 +28,17 @@ export default function Main() {
     summary: 'This gym application incorporates authentication to render different UI and UX depending on the type of user authenticated - gym instructors can create classes, and members can book the classes and leave reviews.',
     link: 'https://github.com/kvlin/gymtrack',
     anchor: 'More info',
-    technologies: 'MySQL, Javascipt Handlebars, Passport.js, Bcrypt',
-    last_technology: 'deployed on Heroku',
+    technologies: 'Node.js, Express, MySQL, JQuery, Handlebars, Passport.js',
+    last_technology: 'Bcrypt',
     images:[{image:gymTrackImg, caption: "Gymtrack - An app for Gym classes creation and reservation", style:{ maxWidth:"1660px", width: "auto",height: "auto"}}]
     
-  }
-  const travlmate = {
-    title: 'Travlmate - Travel information website',
-    summary: 'A one-stop-shop website for travelers to access helpful information that can aid planning for a trip. It saves users time and effort having to search across many sites for a variety of information they might need. Developed with Javascript (JQuery), Bootstrap, HTML and CSS',
-    link: 'https://github.com/kvlin/Travlmate',
-    anchor: 'More info'
   }
   const budgetTracker = {
     title: 'Budget Tracker - Progressive Web App',
     summary: 'The application allows users to log their withdrawals or deposits for budget tracking. It works even when the device is offline and will synchronize the data with the cloud database upon regaining connection. This is particularly useful for travellers who visit regions with spasmodic internet availability.',
     link: 'https://github.com/kvlin/Budget-Tracker',
     anchor: 'More info',
-    technologies: "IndexedDB, Service Worker, Cache API",
+    technologies: "IndexedDB, Service Worker, Cache API, Bootstrap",
     last_technology: 'MongoDB Atlas',
     images:[ 
       {image:budgetTracker1, caption: "Budget Tracker", style:{ maxWidth:"1660px", width: "auto",height: "auto"}}
@@ -53,9 +47,14 @@ export default function Main() {
   }
   const fitnessTracker = {
     title: 'Fitness Tracker - Keeps record of fitness progress',
-    summary: 'This fitness tracker saves the input fitness data (e.g. training time, weight lifted, distance ran) provided by the user and displays them graphically for better visual tracking of their fitness progress. The application was deployed on Heroku and fetches data from MongoDB Atlas.',
+    summary: 'This fitness tracker saves the input fitness data (e.g. training time, weight lifted, distance ran) by the user and displays them graphically for better visual tracking of their fitness progresses.',
     link: 'https://github.com/kvlin/Fitness-Tracker',
-    anchor: 'More info'
+    anchor: 'More info',
+    technologies: "Node.js, Express, MongoDB, Mongoose, Chart.js",
+    last_technology: 'deployed on Heroku',
+    images:[ 
+      {image:budgetTracker1, caption: "Budget Tracker", style:{ maxWidth:"1660px", width: "auto",height: "auto"}}
+    ]
   }
 
   
@@ -74,12 +73,9 @@ export default function Main() {
           <Col style={{marginTop:"2em"}}>
             <ProjectsBrief>{gymTrack}</ProjectsBrief>
           </Col >
-          {/* <Col style={{marginTop:"2em"}}>
+          <Col style={{marginTop:"2em"}}>
             <ProjectsBrief>{fitnessTracker}</ProjectsBrief>
           </Col >
-          <Col style={{marginTop:"2em"}}>
-            <ProjectsBrief>{travlmate}</ProjectsBrief>
-          </Col > */}
 
           {/* <Col style={{marginTop:"2em"}}>
             <ProjectsBrief/>
