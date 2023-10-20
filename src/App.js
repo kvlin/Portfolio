a1924fb86dabca2c09f727da15c1688994c2c9de
kvlin <kevlin129@hotmail.com>
2023-10-20 19:29:43 +1100

    update react version

--- src/App.js
+++ src/App.js
@@ -1,28 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from './pages/Main';
 import CV from './pages/CV';
 import Contact from './pages/Contact';
 import Navbar from './components/Navbar'
-import SkillsBrief from './components/SkillsBrief';
+
 
 function App() {
   return (
     <div>
-      <Navbar/>
+      <Navbar />
       <BrowserRouter>
-        <Switch>
-          <Route path="/" exact >
-            <SkillsBrief/>
-            <Main/>
-          </Route>
-          <Route path="/contact">
-            <Contact/>
-          </Route>
-        </Switch>
+        <Routes>
+          <Route exact path='/' element={<Main />} />
+
+          <Route exact path="/contact" element={<Contact />} />
+
+        </Routes>
       </BrowserRouter>
-    </div>  
+    </div>
   );
 }
 
--- src/components/ProjectsBrief/index.js
+++ src/components/ProjectsBrief/index.js
@@ -1,28 +1,27 @@
-import React, { useState, useEffect }from 'react';
+import React, { useState, useEffect } from 'react';
 import Card from 'react-bootstrap/card'
 import "keen-slider/keen-slider.min.css"
 import 'react-medium-image-zoom/dist/styles.css'
 import { Fancybox, Carousel, Panzoom } from "@fancyapps/ui";
-import "@fancyapps/ui/dist/fancybox.css";
 import "./index.css"
-export default function ProjectsBrief({children}) {
+export default function ProjectsBrief({ children }) {
 
     return (
         <div>
             {/* Create project summary cards */}
             <Card >
-            <a data-caption={children.images[0].caption}  data-fancybox="gallery" href={children.images[0].image} style={children.images[0].style}>
-                <img className="zoom-in" src={children.images[0].image} ></img>
-            </a>
-            <Card.Body>
-                <Card.Title>{children.title}</Card.Title>
-                <Card.Text>
-                {children.summary} Developed using <strong>{children.technologies}</strong> and <strong>{children.last_technology}</strong>.
-                </Card.Text>
-                <Card.Text>
-                <a target="_blank" href={children.link}>{children.anchor}</a>
-                </Card.Text>
-            </Card.Body>
+                <a data-caption={children.images[0].caption} data-fancybox="gallery" href={children.images[0].image} style={children.images[0].style}>
+                    <img className="zoom-in" src={children.images[0].image} ></img>
+                </a>
+                <Card.Body>
+                    <Card.Title>{children.title}</Card.Title>
+                    <Card.Text>
+                        {children.summary} Developed using <strong>{children.technologies}</strong> and <strong>{children.last_technology}</strong>.
+                    </Card.Text>
+                    <Card.Text>
+                        <a target="_blank" href={children.link}>{children.anchor}</a>
+                    </Card.Text>
+                </Card.Body>
             </Card>
         </div>
     )
